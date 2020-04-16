58697f49e49a3dd0fdcf1639af307f030b81ec2f
Pritam Kadam <phkadam2008@gmail.com>
2020-04-16 13:45:37 +0530

    Add release script

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Two arguments are required:"
+    echo "1. Minor Version"
+    echo "2. Major Version"
+    exit 2
+fi
+
+MINOR_VERSION=$1
+MAJOR_VERSION=$1
+
+echo "Tagging MINOR_VERSION: $MINOR_VERSION"
+git tag "$MINOR_VERSION"
+git push origin "$MINOR_VERSION"
+
+echo "Removing MAJOR_VERSION:$MAJOR_VERSION tag if already present"
+git tag -d "$MAJOR_VERSION" || true
+git push origin --delete "$MAJOR_VERSION" || true
+
+echo "Tagging MAJOR_VERSION: $MAJOR_VERSION"
+git tag "$MAJOR_VERSION"
+git push origin "$MAJOR_VERSION"
