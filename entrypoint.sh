6a2015bb7172b5a0a3733f15876f22fd433468a2
Pritam Kadam <phkadam2008@gmail.com>
2020-05-19 20:34:59 +0530

    Lowercase match job status

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-JOB_STATUS=$1
+# Lowercase job status
+JOB_STATUS=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 SLACK_BOT_TOKEN=$2
 CHANNEL=$3
 
@@ -13,21 +14,21 @@ BRANCH=${GITHUB_REF##*/}
 RUN_ID=$GITHUB_RUN_ID
 
 slackMsg() {
-  title=$1
-  color=$2
-  msg="{\"channel\":\"$CHANNEL\", \"attachments\": [ { \"title\":\"$title [ $REPO_NAME ] : [ $WORKFLOW ] : [ $BRANCH ]\", \"text\": \"https://github.com/$REPOSITORY/actions/runs/$RUN_ID\", \"color\": \"$color\" } ]}"
+    title=$1
+    color=$2
+    msg="{\"channel\":\"$CHANNEL\", \"attachments\": [ { \"title\":\"$title [ $REPO_NAME ] : [ $WORKFLOW ] : [ $BRANCH ]\", \"text\": \"https://github.com/$REPOSITORY/actions/runs/$RUN_ID\", \"color\": \"$color\" } ]}"
 }
 
-if [ "$JOB_STATUS" = 'Success' ]; then
-  slackMsg "SUCCESS" "#00FF00"
-elif [ "$JOB_STATUS" = 'Cancelled' ]; then
-  slackMsg "CANCELLED" "#EE6F47"
+if [ "$JOB_STATUS" = 'success' ]; then
+    slackMsg "SUCCESS" "#00FF00"
+    elif [ "$JOB_STATUS" = 'cancelled' ]; then
+    slackMsg "CANCELLED" "#EE6F47"
 else
-  slackMsg "FAILURE" "#FF0000"
+    slackMsg "FAILURE" "#FF0000"
 fi
 
 curl -X POST \
-     -H "Content-type: application/json" \
-     -H "Authorization: Bearer $SLACK_BOT_TOKEN" \
-     -d "$msg" \
-     https://slack.com/api/chat.postMessage
+-H "Content-type: application/json" \
+-H "Authorization: Bearer $SLACK_BOT_TOKEN" \
+-d "$msg" \
+https://slack.com/api/chat.postMessage
