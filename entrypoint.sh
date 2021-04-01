48f538b7ca7e6b6e5eb8f96453f1c3c073686ed3
Pritam Kadam <kpritam@thoughtworks.com>
2021-04-01 23:37:55 +0530

    Support extra args

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,24 @@ JOB_STATUS=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 SLACK_BOT_TOKEN=$2
 CHANNEL=$3
 
+if [[ $# -ge 4 ]]
+then
+    EXTRA_ARGS="${4},"
+else
+    EXTRA_ARGS=
+fi
+
 REPO_NAME=${GITHUB_REPOSITORY##*/}
 REPOSITORY=$GITHUB_REPOSITORY
 WORKFLOW=$GITHUB_WORKFLOW
 BRANCH=${GITHUB_REF##*/}
 RUN_ID=$GITHUB_RUN_ID
 
+
 slackMsg() {
     title=$1
     color=$2
-    msg="{\"channel\":\"$CHANNEL\", \"attachments\": [ { \"title\":\"$title [ $REPO_NAME ] : [ $WORKFLOW ] : [ $BRANCH ]\", \"text\": \"https://github.com/$REPOSITORY/actions/runs/$RUN_ID\", \"color\": \"$color\" } ]}"
+    msg="{\"channel\":\"$CHANNEL\", $EXTRA_ARGS \"attachments\": [ { \"title\":\"$title [ $REPO_NAME ] : [ $WORKFLOW ] : [ $BRANCH ]\", \"text\": \"https://github.com/$REPOSITORY/actions/runs/$RUN_ID\", \"color\": \"$color\" } ]}"
 }
 
 if [ "$JOB_STATUS" = 'success' ]; then
